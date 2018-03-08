b67a0b509fe901ad088889ee75971fa67df5ad02
Kim Blaess <kimblaess@hotmail.com>
2018-03-08 11:35:43 +0100

    add related

--- inc/project_data.php
+++ inc/project_data.php
@@ -11,7 +11,7 @@
 	$project_desc_title = 'Get your User Agent string like a charm'; // goes into a <h2> element
 	$project_desc = 'The what is my User Agent service displays your User Agent by just visiting this page. One common use of the term refers to a web browser telling a website information about the browser and operating system.'; // goes into a <p> element, no html
 
-	$project_referrals = [1,3];
+	$project_referrals = [1,3,4];
 
 	// keywords for this project
 	$project_keywords = array('internet', 'address', 'system', 'browser', 'compatibility', array(
